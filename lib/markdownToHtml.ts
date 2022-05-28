64b03d4ee3d3995f7121b461f0615cf78024a520
ikuma-t <tadokorodev@gmail.com>
2022-05-28 14:19:45 +0900

    feat: support table
    `table` is not CommonMarkdown,
    so this commit adds remark-gfm
    which parse GitHub Flavored Markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,9 @@
 import { remark } from 'remark'
 import html from 'remark-html'
 import prism from 'remark-prism'
+import remarkGfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html, { sanitize: false }).use(prism).process(markdown)
+  const result = await remark().use(html, { sanitize: false }).use(prism).use(remarkGfm).process(markdown)
   return result.toString()
 }
