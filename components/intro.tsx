bb3030f89305ea9d9718585d4303e5b1862a44fb
ikuma-t <tadokorodev@gmail.com>
2022-05-28 16:19:42 +0900

    chore: update style

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Intro = () => {
   return (
     <section className="mt-16 mb-16 md:mb-12">
-      <h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-4xl font-bold tracking-tighter leading-tight pr-8">
         <Link href="/">
           <a className="hover:underline visited:text-gray-900">ikuma-t</a>
         </Link>
