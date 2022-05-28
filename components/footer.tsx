ed507b507c14bc3e8fbe506bc2a7dabf051f411b
ikuma-t <tadokorodev@gmail.com>
2022-05-28 16:19:29 +0900

    style: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="px-24 py-6 flex flex-col lg:flex-row w-full">
+        <div className="px-24 py-6 flex flex-row w-full">
             <a
               href={`/about`}
               className="mx-3 text-sm hover:underline flex-1 text-center"
