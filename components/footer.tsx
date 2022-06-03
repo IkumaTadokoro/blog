9bb9cd2a9526aa8e2efb473fd22e0d298cf8aa5a
ikuma-t <tadokorodev@gmail.com>
2022-06-04 08:46:42 +0900

    fix: add link to Diary

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,17 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="px-24 py-6 flex flex-row w-full">
-            <a
-              href={`/about`}
-              className="mx-3 text-sm hover:underline flex-1 text-center"
-            >
-              About
-            </a>
-            <a
-              href={`https://twitter.com/ikumatdkr`}
-              className="mx-3 text-sm hover:underline flex-1 text-center"
-            >
-              Twitter
-            </a>
-            <a
-              href={`https://github.com/IkumaTadokoro`}
-              className="mx-3 text-sm hover:underline flex-1 text-center"
-            >
-              GitHub
-            </a>
+          <Link href={`/about`}>
+            <a className="mx-3 text-sm hover:underline flex-1 text-center">About</a>
+          </Link>
+          <Link href={`https://ikumatadokoro.github.io/diary`} className="mx-3 text-sm hover:underline flex-1 text-center">
+            <a className="mx-3 text-sm hover:underline flex-1 text-center">Diary</a>
+          </Link>
         </div>
       </Container>
     </footer>
