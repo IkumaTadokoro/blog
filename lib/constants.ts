48fe81fd6a180d9be2581e772103fd24c218c4cd
ikuma-t <tadokorodev@gmail.com>
2022-06-12 00:28:24 +0900

    chore: change ogp path

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const HOME_OG_IMAGE_URL = "https://ikuma-t.work/assets/ogimage.png"
+export const HOME_OG_IMAGE_URL = "https://ikuma-t.work/assets/ogImage.png"
 export const HOME_OG_TITLE = "ikuma-t's blog"
-export const POST_OG_IMAGE_URL = "https://ikuma-t.work/assets/postOgImage.png"
+export const POST_OG_IMAGE_URL = "https://ikuma-t.work/assets/ogImage.png"
