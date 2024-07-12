ed4863f30896076dc57614fa1773416e838acf3e
ikuma-t <tadokorodev@gmail.com>
2024-07-12 21:32:09 +0900

    feat: headerのロゴと流星を実装

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,24 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				'meteor-effect': 'meteor 5s linear infinite',
+			},
+			keyframes: {
+				meteor: {
+					'0%': {
+						transform: 'rotate(215deg) translateX(0)',
+						opacity: '1',
+					},
+					'70%': {
+						opacity: '1',
+					},
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: '0',
+					},
+				},
+			},
 			fontFamily: {
 				sans: ['Lexend', ...defaultTheme.fontFamily.sans],
 			},
