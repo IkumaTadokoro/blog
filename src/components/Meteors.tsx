f70ef1a98eb680c640086a0b0da9203434322cd0
ikuma-t <tadokorodev@gmail.com>
2024-07-14 15:06:49 +0900

    style: 流星の色もテーマに合わせて変更

--- src/components/Meteors.tsx
+++ src/components/Meteors.tsx
@@ -17,6 +17,10 @@ export function Meteors({ number }: MeteorsProps) {
 		max: 0,
 	});
 
+	const [isLightTheme, setIsLightTheme] = useState(
+		document.documentElement.getAttribute('data-theme') === 'light',
+	);
+
 	useEffect(() => {
 		function handler() {
 			setOffset({
@@ -34,6 +38,18 @@ export function Meteors({ number }: MeteorsProps) {
 		};
 	}, []);
 
+	useEffect(() => {
+		function themeChangeHandler() {
+			setIsLightTheme(document.documentElement.getAttribute('data-theme') === 'light');
+		}
+
+		document.addEventListener('theme-change', themeChangeHandler);
+
+		return () => {
+			document.removeEventListener('theme-change', themeChangeHandler);
+		};
+	}, []);
+
 	return (
 		<div className='-z-10 absolute inset-0 h-40 w-full overflow-hidden motion-reduce:hidden'>
 			{meteors.map((_el, idx) => (
@@ -41,8 +57,12 @@ export function Meteors({ number }: MeteorsProps) {
 					aria-hidden={true}
 					key={idx}
 					className={cn(
-						'absolute h-0.5 w-0.5 rotate-[215deg] animate-meteor-effect rounded-full bg-gray-100 shadow-[0_0_0_1px_#ffffff10] motion-reduce:hidden dark:bg-gray-600',
-						'before:-translate-y-[50%] before:absolute before:top-1/2 before:h-0.5 before:w-14 before:transform before:rounded-full before:bg-gradient-to-r before:from-gray-300 before:to-transparent before:content-[""] dark:before:from-gray-600',
+						'absolute h-0.5 w-0.5 rounded-full shadow-[0_0_0_1px_#ffffff10] motion-reduce:hidden',
+						isLightTheme
+							? 'rotate-[45deg] bg-blue-900/30 before:bg-gradient-to-r before:from-blue-900/30'
+							: 'rotate-[215deg] bg-primary/30 before:bg-gradient-to-r before:from-primary/30',
+						'before:-translate-y-[50%] before:absolute before:top-1/2 before:h-0.5 before:w-14 before:transform before:rounded-full before:to-transparent before:content-[""]',
+						'animate-meteor-effect',
 					)}
 					style={{
 						top: `${Math.floor(Math.random() * 400 - 400)}px`,
