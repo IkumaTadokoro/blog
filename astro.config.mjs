81a9bba86e58d80318aa0fb262565461ad305957
ikuma-t <tadokorodev@gmail.com>
2024-07-16 08:05:13 +0900

    chore: astro-pagefind integrationを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import { expressiveCodePlugin, rehypePlugins } from './src/utils/markdown';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon(), expressiveCodePlugin],
+  build: {
+    format: "file",
+  },
+  integrations: [tailwind(), react(), icon(), expressiveCodePlugin, pagefind()],
   markdown: {
     rehypePlugins,
   },
