12facfe57c308c366c2db613549327ae12546d6f
ikuma-t <tadokorodev@gmail.com>
2024-07-15 15:08:57 +0900

    feat: セクションリンクの追加・外部リンクを別タブで開くようにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import { expressiveCodePlugin } from './src/utils/markdown';
+import { expressiveCodePlugin, rehypePlugins } from './src/utils/markdown';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon(), expressiveCodePlugin],
+  markdown: {
+    rehypePlugins,
+  },
 });
--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,6 +1,15 @@
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import expressiveCode from 'astro-expressive-code';
 
+import rehypeAutolinkHeadingsPlugin from 'rehype-autolink-headings';
+import rehypeExternalLinksPlugin from 'rehype-external-links';
+import rehypeSlugPlugin from 'rehype-slug';
+
+import type { RehypePlugins } from 'astro';
+
+import type { Options as RehypeAutolinkHeadingsOptions } from 'rehype-autolink-headings';
+import type { Options as RehypeExternalLinksOptions } from 'rehype-external-links';
+
 /*
  * @see https://expressive-code.com/
  */
@@ -21,3 +30,19 @@ const expressiveCodeOptions = {
 } satisfies Parameters<typeof expressiveCode>[0];
 
 export const expressiveCodePlugin = expressiveCode(expressiveCodeOptions);
+
+export const rehypePlugins: RehypePlugins = [
+	rehypeSlugPlugin,
+	[
+		rehypeAutolinkHeadingsPlugin,
+		{ behavior: 'prepend' } satisfies RehypeAutolinkHeadingsOptions,
+	],
+	[
+		rehypeExternalLinksPlugin,
+		{
+			target: '_blank',
+			rel: ['noopener', 'noreferrer'],
+			content: { type: 'text', value: '🔗' },
+		} satisfies RehypeExternalLinksOptions,
+	],
+];
