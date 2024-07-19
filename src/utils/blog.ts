705b97e9443bdc1eb116d01160d1222ff6b82a22
ikuma-t <tadokorodev@gmail.com>
2024-07-20 07:51:24 +0900

    test: astroアップグレードでvite configがtestを受け付けなくなってしまったので、別ファイルに切り出し
    確かにgetViteConfigに変更はあるものの、これによるものかは不明。

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -47,22 +47,3 @@ export async function buildRssItem(post: CollectionEntry<'blog'>): Promise<RSSFe
 
 	return rssItem;
 }
-
-if (import.meta.vitest) {
-	describe('buildRssItem', async () => {
-		const post = await getEntry('blog', 'test');
-		it('should return a valid RSS item', async () => {
-			const rssItem = await buildRssItem(post);
-
-			expect(rssItem.title).eq('テスト用のサンプル記事');
-			expect(rssItem.description).toContain(
-				'この記事はテスト用に作成した物です。削除しないでください。見出し1',
-			);
-			expect(rssItem.link).eq('/blog/test');
-			expect(rssItem.pubDate).toEqual(new Date('2024-01-01T00:00:00.000Z'));
-			expect(rssItem.content).toContain(
-				'<p>この記事はテスト用に作成した物です。削除しないでください。</p>',
-			);
-		});
-	});
-}
