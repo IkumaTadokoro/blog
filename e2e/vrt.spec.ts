4ba27c02181536d987dfa8ed73c38d1709eb7af5
ikuma-t <tadokorodev@gmail.com>
2025-03-02 09:38:27 +0900

    test: VRTテストを複数ページに対応させ、スナップショットを追加

--- e2e/vrt.spec.ts
+++ e2e/vrt.spec.ts
@@ -1,12 +1,16 @@
 import { expect, test } from '@playwright/test';
 
+const pages = ['/', '/blog', '/blog/markdown-sample', '/talk', '/search'] as const;
+
 test('VRT', async ({ page }) => {
-	await page.goto('/', {
-		waitUntil: 'networkidle',
-	});
-	await expect(page).toHaveScreenshot({
-		fullPage: true,
-		scale: 'device',
-		maxDiffPixelRatio: 0.01,
-	});
+	for (const path of pages) {
+		await page.goto(path, {
+			waitUntil: 'networkidle',
+		});
+		await expect(page).toHaveScreenshot({
+			fullPage: true,
+			scale: 'device',
+			maxDiffPixelRatio: 0.01,
+		});
+	}
 });
